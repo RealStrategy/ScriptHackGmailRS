e535e34be9e2dd1052e221cbb8f24b6af6981f79
RealStrategy <36258164+RealStrategy@users.noreply.github.com>
2018-04-27 19:30:00 -0500

    Add files via upload

---
+++ gmailrs.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+
+#Real Prueba Brute Force
+echo -e "\e[1;33mSCRIPT EJECUTANDO\e[0m" 
+echo
+sleep 5 
+echo -e "\e[1;32m======PASSWORD CRACKING======\e[0m" 
+sleep 3 
+echo 
+echo " ─▄▀▀▀▄──────────────"
+echo " ─█───█──────────────" 
+echo " ███████─────────▄▀▀▄" 
+echo " ██───██░░█▀█▀▀▀▀█░░█" 
+echo " ███▄███░░▀░▀░░░░░▀▀░" 
+echo 
+sleep 3
+        echo " +--------------------------+"
+        echo " |                          |"
+        echo " |      HACK GMAIL RS       |"
+        echo " |                          |"
+        echo " +--------------------------+"
+        echo 
+sleep 3
+        echo -e "\e[1;36m REAL STRATEGY - DOMINANDO\e[0m" 
+sleep 3 
+echo
+        echo -e "\e[1;33m[01]\e[1;32m Instalar hack gmail\e[0m" 
+echo
+        echo -e "\e[1;33m[02]\e[1;32m Informacion de uso\e[0m" 
+echo
+        echo -e "\e[1;33m[03]\e[1;32m Mas Herramientas\e[0m"
+ echo
+        echo -e "\e[1;33m[04]\e[1;32m Creditos al creador\e[0m" 
+echo
+        echo -e "\e[1;33m[05]\e[1;32m Crear lista de contraseñas\e[0m" 
+echo
+        echo -e "\e[1;33m[06]\e[1;32m Ver lista decontraseñas\e[0m" 
+echo
+       echo -e "\e[1;33m[07]\e[1;32m Ejecutar hack gmail\e[0m" 
+echo
+       echo -e "\e[1;33m[08]\e[1;32m Salir del menu\e[0m" 
+echo
+sleep 2
+
+#INICIANDO SCRIPT
+apt update && upgrade -y
+pkg install figlet
+echo -e "\e[1;36m====================================================\e[0m"
+figlet INSTALADO
+echo -e "\e[1;36m====================================================\e[0m"
+echo -e "\e[1;33mDESCARGANDO COMPLEMENTOS... !!!\e[0m"
+sleep 2
+pkg install python2 -y
+sleep 3
+echo
+pkg install git
+echo
+git clone https://github.com/HackeRStrategy/HackGmailRS.git
+echo -e "\e[1;36mINICIANDO HERRAMIENTA...!!!\e[0m"
+sleep 4
+echo
+cd HackGmailRS
+ls
+chmod +x hackgmail.sh
+bash hackgmail.sh
+
